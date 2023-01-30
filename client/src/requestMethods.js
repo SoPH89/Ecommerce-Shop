285dcfc33d9827d4bf28e95d160c34ac7aca7817
SoPH89 <ilanedri1@gmail.com>
2023-01-30 15:18:52 +0200

    adding project

---
+++ client/src/requestMethods.js
@@ -0,0 +1,13 @@
+import { axiosInstance } from "./config";
+
+const BASE_URL = "http://localhost:5000/api";
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZWQ2ZTZjMzk2YzlmOWY3NDEzNDQ5ZSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NDE1NTU5OSwiZXhwIjoxNjQ0NDE0Nzk5fQ.wXl0m54ReEKNEC0V2fEoYHhMmXz63ux0Ych7NSjPbC0";
+
+export const publicRequest = axiosInstance.create({
+    baseURL: BASE_URL,
+})
+
+export const userRequest = axiosInstance.create({
+    baseURL: BASE_URL,
+    header:{token:`Bearer ${TOKEN}`},
+})
